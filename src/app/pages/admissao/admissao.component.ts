f6ee08293603ace62e68f2d7c2b146095bf69c0d
Thiago Prazeres <thiago@Thiagos-Mac-mini.local>
2022-08-02 11:43:57 -0300

    validação do CPF

--- src/app/pages/admissao/admissao.component.ts
+++ src/app/pages/admissao/admissao.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { ColaboradorService } from './../../services/colaborador.service';
 
 @Component({
@@ -12,7 +12,7 @@ export class AdmissaoComponent implements OnInit {
 
   constructor(private colaboradorService: ColaboradorService, private formBuilder: FormBuilder) {
     this.admissaoForm = this.formBuilder.group({
-      cpf: ['', Validators.compose([Validators.required])]
+      cpf: ['', Validators.compose([Validators.required, AdmissaoComponent.ValidaCpf])]
     });
   }
 
@@ -25,6 +25,52 @@ export class AdmissaoComponent implements OnInit {
 
   enviar() {
     console.log(this.admissaoForm.value);
+  }
+
+  static ValidaCpf(controle: AbstractControl) {
+    const cpf = controle.value;
+
+    let soma: number = 0;
+    let resto: number;
+    let valido: boolean;
+
+    const regex = new RegExp('[0-9]{11}');
+
+    if (
+      cpf == '00000000000' ||
+      cpf == '11111111111' ||
+      cpf == '22222222222' ||
+      cpf == '33333333333' ||
+      cpf == '44444444444' ||
+      cpf == '55555555555' ||
+      cpf == '66666666666' ||
+      cpf == '77777777777' ||
+      cpf == '88888888888' ||
+      cpf == '99999999999' ||
+      !regex.test(cpf)
+    )
+      valido = false;
+    else {
+      for (let i = 1; i <= 9; i++)
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      resto = (soma * 10) % 11;
+
+      if (resto == 10 || resto == 11) resto = 0;
+      if (resto != parseInt(cpf.substring(9, 10))) valido = false;
+
+      soma = 0;
+      for (let i = 1; i <= 10; i++)
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      resto = (soma * 10) % 11;
+
+      if (resto == 10 || resto == 11) resto = 0;
+      if (resto != parseInt(cpf.substring(10, 11))) valido = false;
+      valido = true;
+    }
+
+    if (valido) return null;
+
+    return { cpfInvalido: true };
   }
 
 }
